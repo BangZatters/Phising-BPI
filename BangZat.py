284a382413d51367a57886d8b37daf16de15c01a
BangZatters <ddptutor@gmail.com>
2018-08-17 08:49:29 +0700

    OK

---
+++ BangZat.py
@@ -0,0 +1,34 @@
+import os, sys
+
+print ("\033[1;32mIsilah Username & Password")
+username = 'GxDDP'
+password = 'BangZat'
+
+def restart():
+	ngulang = sys.executable
+	os.execl(ngulang, ngulang, *sys.argv)
+
+def main():
+	uname = raw_input("Username : ")
+	if uname == username:
+		pwd = raw_input("Password : ")
+
+		if pwd == password:
+			print "\n\033[1;34mHallo Selamat Datang di Tools BangZat", 
+			sys.exit()
+
+		else:
+			print "\n\033[1;36mMaaf Password Salah\033[00m"
+			print "Log in Kembali\n"
+			restart()
+
+	else:
+		print "\n\033[1;36mMaaf Username Salah\033[00m"
+		print "Log in Kembali\n"
+		restart()
+
+try:
+	main()
+except KeyboardInterrupt:
+	os.system('clear')
+	restart()
